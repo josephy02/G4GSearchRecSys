4984a2c729dc7ca8120add8b981ec2eae23b2859
joseph02 <joseph02@uw.edu>
2024-12-05 15:43:39 -0800

    ha

--- search.php
+++ search.php
@@ -6,53 +6,55 @@
 <body>
 
 <h1>My Search</h1>
-<p>Here's a UI that searches through an index of documents from web crawling on the Allen Institute's official $
-<p>Given that the Allen Institute is famous for its groundbreaking research, some interesting queries to try ar$
+<p>Here's a UI that searches through an index of documents from web crawling on geeks4geeks </p>
+<p>....</p>
 <form action="search.php" method="post">
-        <input type="text" size=40 name="search_string" value="<?php echo $_POST["search_string"];?>"/>
-        <input type="submit" value="Search"/>
+    <input type="text" size=40 name="search_string" value="<?php echo $_POST["search_string"];?>"/>
+    <input type="submit" value="Search"/>
 </form>
 
 <?php
-     	if (isset($_POST["search_string"])) {
-                $search_string = $_POST["search_string"];
+  if (isset($_POST["search_string"])) {
+    $search_string = $_POST["search_string"];
 
-                file_put_contents("logs.txt", $search_string.PHP_EOL, FILE_APPEND | LOCK_EX);
+    file_put_contents("logs.txt", $search_string.PHP_EOL, FILE_APPEND | LOCK_EX);
 
-                $qfile = fopen("query.py", "w");
-                fwrite($qfile, "import pyterrier as pt\nif not pt.started():\n\tpt.init()\n\n");
-                fwrite($qfile, "import pandas as pd\nqueries = pd.DataFrame([[\"q1\", \"$search_string\"]], col$
-                fwrite($qfile, "index = pt.IndexFactory.of(\"./ai_index/\")\n");
-                fwrite($qfile, "tf_idf = pt.BatchRetrieve(index, wmodel=\"BM25\")\n");
-                fwrite($qfile, "results = tf_idf.transform(queries)\n");
+    $qfile = fopen("query.py", "w");
 
-                for ($i=0; $i<5; $i++) {
-                        fwrite($qfile, "print(index.getMetaIndex().getItem(\"filename\",results.docid[$i]))\n");
-                        fwrite($qfile, "if index.getMetaIndex().getItem(\"title\", results.docid[$i]).strip() !$
-                        fwrite($qfile, "\tprint(index.getMetaIndex().getItem(\"title\",results.docid[$i]))\n");
-                        fwrite($qfile, "else:\n\tprint(index.getMetaIndex().getItem(\"filename\",results.docid[$
-                }
-                fclose($qfile);
+    fwrite($qfile, "import pyterrier as pt\nif not pt.started():\n\tpt.init()\n\n");
+    fwrite($qfile, "import pandas as pd\nqueries = pd.DataFrame([[\"q1\", \"$search_string\"]], columns=[\"qid\",\"query\"])\n");
+    fwrite($qfile, "index = pt.IndexFactory.of(\"./geek_index/\")\n");
+    fwrite($qfile, "tf_idf = pt.BatchRetrieve(index, wmodel=\"BM25\")\n");
+    fwrite($qfile, "results = tf_idf.transform(queries)\n");
 
-                exec("ls | nc -u 127.0.0.1 10019");
-                sleep(3);
+    for ($i=0; $i<5; $i++) {
+      fwrite($qfile, "print(index.getMetaIndex().getItem(\"filename\",results.docid[$i]))\n");
+      fwrite($qfile, "if index.getMetaIndex().getItem(\"title\", results.docid[$i]).strip() != \"\":\n");
+      fwrite($qfile, "\tprint(index.getMetaIndex().getItem(\"title\",results.docid[$i]))\n");
+      fwrite($qfile, "else:\n\tprint(index.getMetaIndex().getItem(\"filename\",results.docid[$i]))\n");
+    }
 
-                $stream = fopen("output", "r");
+    fclose($qfile);
 
-                $line=fgets($stream);
+    exec("ls | nc -u 127.0.0.1 10019");
+    sleep(3);
 
-                while(($line=fgets($stream))!=false) {
-                        $clean_line = preg_replace('/\s+/',',',$line);
-                        $record = explode("./", $clean_line);
-                        $line = fgets($stream);
-                        echo "<a href=\"http://$record[1]\">".$line."</a><br/>\n";
-                }
+    $stream = fopen("output", "r");
 
-                fclose($stream);
+    $line=fgets($stream);
 
-                exec("rm query.py");
-                exec("rm output");
-                }
+    while(($line=fgets($stream))!=false) {
+      $clean_line = preg_replace('/\s+/',',',$line);
+      $record = explode("./", $clean_line);
+      $line = fgets($stream);
+      echo "<a href=\"http://$record[1]\">".$line."</a><br/>\n";
+    }
+
+    fclose($stream);
+
+    exec("rm query.py");
+    exec("rm output");
+    }
 ?>
 </body>
 </html>
